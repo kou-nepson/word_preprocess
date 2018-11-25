47ad82f01ec52a54f3c81faf1227f2516f136e8a
杉山航 <cjgjc803@gmail.com>
2018-11-25 18:04:34 +0900

    上がってないやつ２

--- ppmi.py
+++ ppmi.py
@@ -3,7 +3,7 @@ sys.path.append('..')
 import numpy as np
 from preprocess import preprocess, create_co_matrix, cos_similarity, ppmi
 
-text = 'You say goodbye and I say hello.'
+text = "Tomoki is dog."
 corpus, word_to_id, id_to_word = preprocess(text)
 vocab_size = len(word_to_id)
 C = create_co_matrix(corpus, vocab_size)
